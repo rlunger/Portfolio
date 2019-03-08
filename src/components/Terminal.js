d1127bba8537223c033b6060c32dd55f9e61bf7f
Ryan Lunger <ryanlunger@gmail.com>
2019-03-08 12:56:17 -0600

    Code cleanup changes.

--- src/components/Terminal.js
+++ src/components/Terminal.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import Console from './Console'
-import Explorer from './Explorer'
 import scanlines from './scanlines.jpg'
-import MonitorFrame from './MonitorFrame'
 
 const StyledTerminal = styled.div`
   display: flex;
@@ -20,47 +18,6 @@ const StyledTerminal = styled.div`
   font-family: 'Fira Code', Consolas;
   @import url('https://fonts.googleapis.com/css?family=Lobster');
 `
-
-/*
-  &::after {
-    content: ' ';
-    display: block;
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    background: rgba(18, 16, 16, 0.1);
-    opacity: 0;
-    z-index: 2;
-    pointer-events: none;
-    animation: flicker 0.15s infinite;
-  }
-  &::before {
-    content: ' ';
-    display: block;
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    background: linear-gradient(
-        rgba(18, 16, 16, 0) 50%,
-        rgba(0, 0, 0, 0.25) 50%
-      ),
-      linear-gradient(
-        90deg,
-        rgba(255, 0, 0, 0.06),
-        rgba(0, 255, 0, 0.02),
-        rgba(0, 0, 255, 0.06)
-      );
-    z-index: 2;
-    background-size: 100% 2px, 3px 100%;
-    pointer-events: none;
-  }
-  .crt {
-  }
-  */
 
 const Terminal = props => (
   <StyledTerminal>
